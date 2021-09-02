aa32671ed5c5fc2a6ff0b07e2e4a2325550dc377
kartikeya <calculus783@gmail.com>
2021-09-02 19:44:09 +0530

    add repositories

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -8,7 +8,5 @@ public class Spring5RecipeAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5RecipeAppApplication.class, args);
-
-		System.out.println("application running");
 	}
 }
